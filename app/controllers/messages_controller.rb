6f235f40a1a5cb8b63ffa9fb97148e1ba29642e2
limasolTK <takana1997@gmail.com>
2019-06-04 16:22:05 +0900

    フロント部分の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,18 @@ class MessagesController < ApplicationController
   def index
     
   end
+
+  def new
+    @message = Message.new    
+  end
+
+  def create
+    @message = Message.new(create_params)
+    @message.save
+  end
+
+  private
+  def create_params
+    params.require(:message).permit
+  end
 end
