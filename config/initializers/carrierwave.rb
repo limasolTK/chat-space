482f31a2a9c353cb18c812c671f4a6cc100d5e07
limasolTK <takana1997@gmail.com>
2019-06-14 17:29:04 +0900

    s3新規作成

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'upoload-box'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upoload-box'
+  config.fog_directory  = 'uppload-box'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/uppload-box'
 end
